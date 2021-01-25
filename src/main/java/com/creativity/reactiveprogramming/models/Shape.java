a3bbae934e339571beeec27b910b32918175f2f9
Hansel <hansel.hir@gmail.com>
2021-01-25 17:23:16 -0500

    Docs: Clase Shape

---
+++ src/main/java/com/creativity/reactiveprogramming/models/Shape.java
@@ -0,0 +1,9 @@
+package com.creativity.reactiveprogramming.models;
+
+import lombok.*;
+
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor @ToString
+public class Shape {
+    private String shape;
+    private String color;
+}
