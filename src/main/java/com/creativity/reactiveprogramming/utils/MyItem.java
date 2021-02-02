51a3341a8b066db0e06116c375dff77f5aeec27e
Hansel <hansel.hir@gmail.com>
2021-02-02 12:19:32 -0500

    Docs: Clase de prueba.

---
+++ src/main/java/com/creativity/reactiveprogramming/utils/MyItem.java
@@ -0,0 +1,11 @@
+package com.creativity.reactiveprogramming.utils;
+
+public class MyItem {
+
+    public int id;
+
+    public MyItem(int id) {
+        this.id = id;
+        System.out.println("Construyendo MyItem " + id);
+    }
+}
